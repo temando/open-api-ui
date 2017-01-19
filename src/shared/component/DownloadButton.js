f08435b2cf45b63dd30646af7ee369f856c04c7e
Khoa Tran <khoatran@temando.com>
2017-01-19 16:40:36 +1000

    fix issue with scrolling

--- src/shared/component/DownloadButton.js
+++ src/shared/component/DownloadButton.js
@@ -2,10 +2,22 @@ import React, { PropTypes, Component } from 'react';
 
 export default class DownloadButton extends Component {
   render() {
+    // return (
+    //   <div>
+    //     <a target="_blank" href={this.props.url}>Download Swagger</a>
+    //   </div>
+    // );
     return (
       <div>
-        <a target="_blank" href={this.props.url}>Download Swagger</a>
+        {/*<a href="#store">Go to store</a>*/}
+        <button onClick={() => {
+          window.location.replace(
+            window.location.pathname + window.location.search + '#/store'
+          );
+        }}>Go to store
+        </button>
       </div>
+
     );
   }
 }
